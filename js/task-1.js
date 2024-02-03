a080b55fdc84187c9b8dc7e38b0847bee39aa849
Khamala <elektrikamegawolt@gmail.com>
2024-02-03 15:59:24 +0100

    task-1, task-2

--- js/task-1.js
+++ js/task-1.js
@@ -2,14 +2,8 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
 
     const totalPrice = quantity * pricePerDroid;
 
-    if (totalPrice > customerCredits) {
-
-        return "Insufficient funds!";
-
-    } else {
-
-        return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`
-    };
+    return totalPrice > customerCredits ? "Insufficient funds!" : `"You ordered ${quantity} droids worth ${totalPrice} credits!"`
+    
 };
 
 
